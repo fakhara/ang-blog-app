3b3bfd5f6c24723a9a3e07779be9a0d5028595db
Fakhara <fakhara.imran82@gmail.com>
2024-01-19 14:10:48 +0100

    add components in project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleCatagoryComponent } from './pages/single-catagory/single-catagory.component';
+import { SinglePostComponent } from './pages/single-post/single-post.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
+import { TermsAndConditionComponent } from './pages/terms-and-condition/terms-and-condition.component';
+import { ContactUsComponent } from './pages/contact-us/contact-us.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   { path:'', component: HomeComponent},
+   { path: 'caregory', component: SingleCatagoryComponent},
+   { path: 'post', component: SinglePostComponent},
+
+   { path: 'about', component: AboutUsComponent},
+   { path: 'term-conditions', component: TermsAndConditionComponent},
+   { path: 'contact', component: ContactUsComponent}
+
+   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/pages/single-catagory/single-catagory.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-catagory',
+  templateUrl: './single-catagory.component.html',
+  styleUrl: './single-catagory.component.css'
+})
+export class SingleCatagoryComponent {
+
+}
